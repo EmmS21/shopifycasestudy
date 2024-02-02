4fb1270baaaf6a4589080e9e0b1dc3dcc335f1a7
EmmS21 <44911198+EmmS21@users.noreply.github.com>
2024-02-02 15:32:31 -0800

    optimizing indices in cache

--- shopify_backend/app/controllers/items_controller.rb
+++ shopify_backend/app/controllers/items_controller.rb
@@ -1,50 +1,57 @@
 class ItemsController < ApplicationController
-    def index
-        cache_key = "items/all"
-        items = Rails.cache.fetch(cache_key, expires_in: 5.minutes) do
-            Item.all.to_a
-        end
-        render json: items
+  before_action :set_cache_key, only: [:index]
+  before_action :set_item_cache_key, only: [:create, :update, :destroy]
+  
+  def index
+    items = Rails.cache.fetch(@cache_key, expires_in: 5.minutes) do
+      Item.where(category: params[:category]).to_a
     end
-    #   items = Item.all
-    #   render json: items
-    # end
-  
-    def create
-      item = Item.new(item_params)
-      if item.save
-        Rails.cache.delete("items/all")
-        render json: item, status: :created
-        # render json: item, status: :created
-      else
-        render json: item.errors, status: :unprocessable_entity
-      end
-    end
-  
-    def update
-      item = Item.find(params[:id])
-      if item.update(item_params)
-        Rails.cache.delete("items/all")
-        render json: item
-      else
-        render json: item.errors, status: :unprocessable_entity
-      end
-    end
-  
-    def destroy
-      item = Item.find(params[:id])
-      if item.destroy
-        Rails.cache.delete("items/all")
-        head :no_content
-      else
-        render json: { error: "Item could not be deleted" }, status: :unprocessable_entity
-      end
-    end
-  
-    private
-  
-    def item_params
-      params.require(:item).permit(:name, :description)
+    render json: items
+  end
+
+  def create
+    item = Item.new(item_params)
+    if item.save
+      Rails.cache.delete(@item_cache_key) 
+      render json: item, status: :created
+    else
+      render json: item.errors, status: :unprocessable_entity
     end
   end
-  
+
+  def update
+    item = Item.find(params[:id])
+    if item.update(item_params)
+      Rails.cache.delete(@item_cache_key) # Invalidate cache for specific category
+      render json: item
+    else
+      render json: item.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    item = Item.find(params[:id])
+    if item.destroy
+      Rails.cache.delete(@item_cache_key) # Invalidate cache for specific category
+      head :no_content
+    else
+      render json: { error: "Item could not be deleted" }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :category)
+  end
+
+  def set_cache_key
+    # Use a cache key that includes the category parameter
+    @cache_key = "items/#{params[:category]}/all"
+  end
+
+  def set_item_cache_key
+    # Use a cache key that includes the category parameter
+    @item_cache_key = "items/#{params[:category]}/all"
+  end
+end
--- shopify_backend/db/schema.rb
+++ shopify_backend/db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_01_042432) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_02_230659) do
   create_table "items", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "category"
   end
 
 end
